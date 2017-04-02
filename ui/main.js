ddd8096f71ae77b39400c07dd99e5ddb621d41f7
Shwetima <shwetimasakshi12@gmail.com>
2017-04-02 11:55:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,12 @@
 console.log('Loaded!');
+//change the text
 var element=document.getElementById('main-text');
 element.innerHTML='new value';
+//move the image
+var element = document.getElementById('madi');
+img.onclick= function(){
+    img.style.marginLeft='100px';
+    
+};
+
+
